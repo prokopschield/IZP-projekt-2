3ba348c9a016f705fcaf8ed06068f4df03153247
Prokop <prokop.schield@protonmail.com>
2021-11-08 23:33:39 +0100

    build.c: modify heading

--- build.c
+++ build.c
@@ -11,7 +11,7 @@ void pipecmd(char* restrict command, FILE* f) {
 int main() {
 	FILE *setcal = fopen("setcal.c", "w");
 	fprintf(setcal, "/* set calculator by { xkrama07, xpekni01, xschie03, xstipe02 } */\n");
-	fprintf(setcal, "/* K dispozici na https://cdn.nodesite.eu/static/");
+	fprintf(setcal, "/* https://cdn.nodesite.eu/static/");
 	pipecmd("node store", setcal);
 	fprintf(setcal, ".html */\n");
 	fprintf(setcal, "\n");
