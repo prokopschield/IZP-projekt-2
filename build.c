dc282069e3d418702bc7ac9d4f1dae1804e43767
Prokop <prokop.schield@protonmail.com>
2021-11-08 23:30:16 +0100

    build.c: add xlogins

--- build.c
+++ build.c
@@ -10,7 +10,7 @@ void pipecmd(char* restrict command, FILE* f) {
 
 int main() {
 	FILE *setcal = fopen("setcal.c", "w");
-	fprintf(setcal, "/* set calculator by xschie03 */\n");
+	fprintf(setcal, "/* set calculator by { xkrama07, xpekni01, xschie03, xstipe02 } */\n");
 	fprintf(setcal, "/* K dispozici na https://cdn.nodesite.eu/static/");
 	pipecmd("node store", setcal);
 	fprintf(setcal, ".html */\n");
