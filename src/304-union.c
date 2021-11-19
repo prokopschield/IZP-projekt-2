20fec866be1ea735ca28028388d51a99ea26124f
stefanpeknik <stefa9982@seznam.cz>
2021-11-19 15:42:13 +0100

    304-union.c: vytvoreno ale neodzkouseno

--- src/304-union.c
+++ src/304-union.c
@@ -7,20 +7,20 @@
 #include <string.h>
 
 void Command_union (set_t *set1, set_t *set2) {
-    for (int i = 0; i < set1->number_of_elements; i++) {
-        printf("%s ", set1->elements[i]);
+    for (int i = 0; i < (int)set1->number_of_elements; i++) {
+        printf("%s ", set1->elements[i]->str.data);
     }
 
     bool print;
-    for (int i = 0; i < set2->number_of_elements; i++) {
+    for (int i = 0; i < (int)set2->number_of_elements; i++) {
         print = true;
-        for (int j = 0; j < set1->number_of_elements; j++) {
-            if (strcmp(set2->elements[i], set1->elements[j])) {
+        for (int j = 0; j < (int)set1->number_of_elements; j++) {
+        if (strcmp((const char*)set2->elements[i]->str.data, (const char*)set1->elements[j]->str.data)) {
                 print = false;
                 break;
             }
         }
         if (print)
-            printf("%s ", set2->elements[i]);
+            printf("%s ", set2->elements[i]->str.data);
     }
 }
