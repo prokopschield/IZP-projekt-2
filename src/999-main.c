f5e723ca55d6e93a8e3617c9ad30d4bb05a4b6e4
stefanpeknik <stefa9982@seznam.cz>
2021-11-19 15:42:25 +0100

    999-main.c: zkouska implementovanych metod

--- src/999-main.c
+++ src/999-main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
 int main() {
+
+    set_t set1;
+    set1.line = 1;
+    set1.number_of_elements = 0;
+
+//    printf("%s", set1.elements[0]->str.data);
+
+    if(Command_empty(&set1))
+        puts("true");
+    else
+        puts("false");
+
+    Command_card(&set1);
+
 	// Remember to free everything!!!
 	mem_free_everything();
 }
