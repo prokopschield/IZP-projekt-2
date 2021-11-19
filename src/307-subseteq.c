e2bb1c169f9b1f64c73d1236a0806620da5572fc
stefanpeknik <stefa9982@seznam.cz>
2021-11-19 21:26:03 +0100

    307-subseteq.c: added

--- src/307-subseteq.c
+++ src/307-subseteq.c
@@ -2,3 +2,32 @@
 // Created by Štefan Pekník on 19.11.2021.
 //
 
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+void Command_subseteq(set_t *set1, set_t *set2) {
+    int count = 0;
+    for (int i = 0; i < (int) set1->number_of_elements; i++) {
+        int equals = false;
+        for (int j = 0; j < (int) set2->number_of_elements; j++) {
+            if (strcmp((const char *) set1->elements[i]->str.data, (const char *) set2->elements[j]->str.data)) {
+                equals = true;
+            }
+        }
+        if (equals) {
+            count++;
+        }
+        else {
+            printf("false\n");
+            return;
+        }
+    }
+    if (set1->number_of_elements == set2->number_of_elements) {
+        printf("false\n");
+        return;
+    }
+
+    printf("true\n");
+}
