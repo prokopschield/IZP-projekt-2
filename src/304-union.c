4fe3e599453b4a934284886cd2e1b8bb1be01860
stefanpeknik <stefa9982@seznam.cz>
2021-11-19 21:25:53 +0100

    304-union.c: eddited

--- src/304-union.c
+++ src/304-union.c
@@ -11,9 +11,8 @@ void Command_union (set_t *set1, set_t *set2) {
         printf("%s ", set1->elements[i]->str.data);
     }
 
-    bool print;
     for (int i = 0; i < (int)set2->number_of_elements; i++) {
-        print = true;
+        bool print = true;
         for (int j = 0; j < (int)set1->number_of_elements; j++) {
         if (strcmp((const char*)set2->elements[i]->str.data, (const char*)set1->elements[j]->str.data)) {
                 print = false;
