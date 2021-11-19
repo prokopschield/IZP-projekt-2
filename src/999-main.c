4a25adfa2f787411c689ddadccd32d46adf9625b
stefanpeknik <stefa9982@seznam.cz>
2021-11-19 21:36:27 +0100

    999-main.c: fix main

--- src/999-main.c
+++ src/999-main.c
@@ -2,21 +2,6 @@
 
 int main() {
 
-
-
-    set_t set1 = { 0, 1, NULL };
-
-
-
-//    printf("%s", set1.elements[0]->str.data);
-
-    if(Command_empty(&set1))
-        puts("true");
-    else
-        puts("false");
-
-    Command_card(&set1);
-
 	// Remember to free everything!!!
 	mem_free_everything();
 }
