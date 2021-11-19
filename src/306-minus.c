1b8523560a73051e66eff62a1e2b3436bbe4bb21
stefanpeknik <stefa9982@seznam.cz>
2021-11-19 21:26:00 +0100

    306-minus.c: added

--- src/306-minus.c
+++ src/306-minus.c
@@ -2,3 +2,21 @@
 // Created by Štefan Pekník on 19.11.2021.
 //
 
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+void Command_minus (set_t *set1, set_t *set2) {
+    for (int i = 0; i < (int)set1->number_of_elements; i++) {
+        bool print = true;
+        for (int j = 0; j < (int) set2->number_of_elements; j++) {
+            if (strcmp((const char *) set1->elements[i]->str.data, (const char *) set2->elements[j]->str.data)) {
+                print = false;
+                break;
+            }
+        }
+        if (print)
+            printf("%s ",set1->elements[i]->str.data);
+    }
+}
