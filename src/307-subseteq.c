f34255fae59a69ee6704305d07237f4ae09006b6
stefanpeknik <stefa9982@seznam.cz>
2021-11-19 21:56:06 +0100

    307-subseteq.c: edit name

--- src/307-subseteq.c
+++ src/307-subseteq.c
@@ -7,7 +7,7 @@
 #include <stdio.h>
 #include <string.h>
 
-void Command_subseteq(set_t *set1, set_t *set2) {
+void Cmd_subseteq(set_t *set1, set_t *set2) {
     int count = 0;
     for (int i = 0; i < (int) set1->number_of_elements; i++) {
         int equals = false;
