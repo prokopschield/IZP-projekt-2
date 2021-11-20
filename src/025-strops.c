fc0eede8b9b2af5dceb117645fd0e6e5fd4c1961
Prokop <prokop.schield@protonmail.com>
2021-11-20 13:48:05 +0100

    025-strops.c: improve performance

--- src/025-strops.c
+++ src/025-strops.c
@@ -2,9 +2,11 @@
 #include <stdio.h>
 
 void strop_skip_ws(string_t* restrict str, int* index) {
-	while (!(str->data[*index] != ' ' && str->data[*index] != '\t' && str->data[*index] != '\r' && str->data[*index] != '\n')) {
-		++*index;
-		if (*index >= (int) str->len) {
+	register byte_t c;
+	register const int len = (int) str->len;
+	while ((c = str->data[*index]),
+				 (c == ' ' || c == '\t' || c == '\r' || c == '\n')) {
+		if (++(*index) >= len) {
 			*index = EOF;
 			return;
 		}
@@ -15,7 +17,9 @@ string_t strop_read_word(string_t* restrict str, int* index) {
 	strop_skip_ws(str, index);
 	int i = *index;
 	int l = 0;
-	while (str->data[i + l] > ' ') ++l;
+	while (str->data[i + l] > ' ') {
+		++l;
+	}
 	string_t substr = str_alloc(l);
 	for (int j = 0; j < l; ++j) {
 		substr.data[j] = str->data[(*index)++];
@@ -25,8 +29,10 @@ string_t strop_read_word(string_t* restrict str, int* index) {
 }
 
 bool str_equal(string_t* left, string_t* right) {
-	if (left == right) return true;
-	if (left->len != right->len) return false;
+	if (left == right)
+		return true;
+	if (left->len != right->len)
+		return false;
 	for (size_t i = 0, l = right->len; i < l; ++i) {
 		if (left->data[i] != right->data[i]) {
 			return false;
