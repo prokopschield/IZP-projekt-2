d4da03b08c3178322e927bb298b88e7cc24bc6da
Prokop <prokop.schield@protonmail.com>
2021-11-22 12:47:16 +0100

    130-set.c: print U when printing Universe

--- src/775-execute.c
+++ src/775-execute.c
@@ -2,9 +2,9 @@ void execute(array_t* error, array_t* lines, size_t* line_i, FILE* output) {
 	line_t* line = lines->items[*line_i];
 	const bool bval = line->val_cmd && eval(error, lines->items[1], line);
 	if (line->val_set) {
-		set_print(line->val_set, output);
+		set_print(line->val_set, output, *line_i == 1);
 	} else if (line->val_rel) {
-		rel_print(line->val_rel, output);
+		rel_print(line->val_rel, output, 0);
 	} else {
 		fputs(bval ? "true\n" : "false\n", output);
 	}
