0076fdcc6a23347696d297336b2afbaef05eb05a
stefanpeknik <stefa9982@seznam.cz>
2021-11-22 01:12:40 +0100

    305-intersect.c: use better method for searching element

--- src/305-intersect.c
+++ src/305-intersect.c
@@ -8,10 +8,8 @@
 set_t* Cmd_intersect (set_t *set1, set_t *set2) {
     set_t* seti = empty_set();
     for (int i = 0; i < (int) set1->number_of_elements; i++) {
-        for (int j = 0; j < (int) set2->number_of_elements; j++) {
-            if (set1->elements[i] == set2->elements[j]) {
-                add_element_to_set(set1->elements[i], &seti);
-            }
+        if (is_element_in_set(set1->elements[i], set2)) {
+            add_element_to_set(set1->elements[i], &seti);
         }
     }
     return seti;
