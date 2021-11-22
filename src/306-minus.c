1f1b37ba6eb4bac149399a1a0bb8fcba3b230304
stefanpeknik <stefa9982@seznam.cz>
2021-11-22 11:13:55 +0100

    306-minus.c: rework method

--- src/306-minus.c
+++ src/306-minus.c
@@ -9,14 +9,7 @@
 set_t* Cmd_minus (set_t *set1, set_t *set2) {
     set_t* setm = empty_set();
     for (int i = 0; i < (int)set1->number_of_elements; i++) {
-        bool add = true;
-        for (int j = 0; j < (int)set2->number_of_elements; j++) {
-            if (set1->elements[i] == set2->elements[j]) {
-                add = false;
-                break;
-            }
-        }
-        if (add)
+        if (!is_element_in_set(set1->elements[i], &set2))
             add_element_to_set(set1->elements[i], &setm);
     }
     return setm;
