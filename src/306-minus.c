b60f727d92ed23db35a1790da548a19404a0f52a
stefanpeknik <stefa9982@seznam.cz>
2021-11-22 11:23:37 +0100

    306-minus.c: fix error

--- src/306-minus.c
+++ src/306-minus.c
@@ -9,7 +9,7 @@
 set_t* Cmd_minus (set_t *set1, set_t *set2) {
     set_t* setm = empty_set();
     for (int i = 0; i < (int)set1->number_of_elements; i++) {
-        if (!is_element_in_set(set1->elements[i], &set2))
+        if (!is_element_in_set(set1->elements[i], set2))
             add_element_to_set(set1->elements[i], &setm);
     }
     return setm;
