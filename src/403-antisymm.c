9f9ad996034e96c5bfc0a639bc020d464629d720
Jurgee <jiristipek@email.cz>
2021-11-30 17:28:20 +0100

    Finish method

--- src/403-antisymm.c
+++ src/403-antisymm.c
@@ -2,3 +2,28 @@
 // Created by jiris on 30/11/2021.
 //
 
+#include <stdbool.h>
+#include <stdio.h>
+
+bool Cmd_antisymm(rel_t *rel1){
+
+    bool x;
+    for (size_t i = 0; i < rel1->number_of_pairs; i++) {
+        x = false;
+        if (rel1->pairs[i]->left == rel1->pairs[i]->right) {
+            x = true;
+        }
+        else {
+            for (size_t j = i; j < rel1->number_of_pairs; j++) {
+                if ((rel1->pairs[i]->left == rel1->pairs[j]->right && rel1->pairs[i]->right == rel1->pairs[j]->left)) {
+                    x = false;
+                    break;
+                }
+            }
+        }
+        if (!x) {
+            return false;
+        }
+    }
+    return true;
+}
