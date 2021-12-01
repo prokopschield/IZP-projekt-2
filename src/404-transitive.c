c16a857a434227fc8659c657f4ae5da119e9f9c4
stefanpeknik <stefa9982@seznam.cz>
2021-12-01 17:36:44 +0100

    404-transitive.c: finish reworking method

--- src/404-transitive.c
+++ src/404-transitive.c
@@ -4,27 +4,12 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-bool find_pair(rel_t* rel1, element_t* left, element_t* right) {
-    for (size_t i = 0; i < rel1->number_of_pairs; i++) {
-        if (rel1->pairs[i]->left == left && rel1->pairs[i]->right == right)
-            return true;
-    }
-    return false;
-}
-
-bool Cmd_transitive(rel_t *rel1){
-    bool found;
-
-    for (size_t i = 0; i < rel1->number_of_pairs; i++) {
-        found = false;
-        for (size_t j = 0; j < rel1->number_of_pairs; j++) {
-            if (rel1->pairs[i]->right == rel1->pairs[j]->left && find_pair(rel1, rel1->pairs[i]->left, rel1->pairs[j]->right)) {
-                found = true;
-                break;
-            }
-        }
-        if (!found)
-            return false;
-    }
-    return true;
+bool Cmd_transitive(rel_t* rel1) {
+	for (size_t i = 0; i < rel1->number_of_pairs; i++) {
+		if (!is_pair_in_rel(get_pair(rel1->pairs[i]->left, rel1->pairs[i]->left), rel1))
+			return false;
+		if (!is_pair_in_rel(get_pair(rel1->pairs[i]->right, rel1->pairs[i]->right), rel1))
+			return false;
+	}
+	return true;
 }
