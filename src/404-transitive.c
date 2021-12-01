f181f4097500b9ddfa265da2d53a20ad9a36beff
stefanpeknik <stefa9982@seznam.cz>
2021-12-01 23:07:01 +0100

    404-transitive.c:  rework method using rel.c and pairs.c

--- src/404-transitive.c
+++ src/404-transitive.c
@@ -4,12 +4,14 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-bool Cmd_transitive(rel_t* rel1) {
-	for (size_t i = 0; i < rel1->number_of_pairs; i++) {
-		if (!is_pair_in_rel(get_pair(rel1->pairs[i]->left, rel1->pairs[i]->left), rel1))
-			return false;
-		if (!is_pair_in_rel(get_pair(rel1->pairs[i]->right, rel1->pairs[i]->right), rel1))
-			return false;
+bool Cmd_transitive(rel_t* rel) {
+	for (size_t i = 0; i < rel->number_of_pairs; i++) {
+        for (size_t j = 0; j < rel->number_of_pairs; j++) {
+            if (rel->pairs[i]->right == rel->pairs[j]->left) {
+                if (!is_pair_in_rel(get_pair(rel->pairs[i]->left, rel->pairs[j]->right), rel))
+                    return false;
+            }
+        }
 	}
 	return true;
 }
