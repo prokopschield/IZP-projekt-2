5036d9a87cc2991ba8bbbc7a1f4a261e60dae445
stefanpeknik <stefa9982@seznam.cz>
2021-12-01 23:06:52 +0100

    401-reflexive.c: rework method using rel.c and pairs.c

--- src/401-reflexive.c
+++ src/401-reflexive.c
@@ -7,38 +7,12 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-bool contains_element(element_t** elements ,element_t* element, size_t num_of_els) {
-    for (size_t i = 0; i < num_of_els; i++) {
-        if (elements[i] == element) return true;
-    }
-    return false;
-}
-
 bool Cmd_reflexive (rel_t* rel) {
-    element_t** elements = malloc(sizeof(element_t*) * rel->number_of_pairs * 2);
-    size_t num_of_els = 0;
-
     for (size_t i = 0; i < rel->number_of_pairs; i++) {
-        if (contains_element(elements, rel->pairs[i]->left, num_of_els)) {
-            elements[num_of_els] = rel->pairs[i]->left;
-            num_of_els++;
-        }
-        if (contains_element(elements, rel->pairs[i]->right, num_of_els)) {
-            elements[num_of_els] = rel->pairs[i]->right;
-            num_of_els++;
-        }
+        if (!is_pair_in_rel(get_pair(rel->pairs[i]->left, rel->pairs[i]->left), rel))
+            return false;
+        if (!is_pair_in_rel(get_pair(rel->pairs[i]->right, rel->pairs[i]->right), rel))
+            return false;
     }
-
-    size_t counter = 0;
-    for (size_t i = 0; i < rel->number_of_pairs; i++) {
-        if (rel->pairs[i]->left == rel->pairs[i]->right)
-            counter++;
-    }
-
-    free(elements);
-
-    if (num_of_els == counter)
-        return true;
-
-    return false;
+    return true;
 }
