cef44930ec1aac7845bcc9075f5e15caef51cd84
Prokop <prokop.schield@protonmail.com>
2021-12-02 21:32:18 +0100

    120-pair.c: add malloc checking

--- src/120-pair.c
+++ src/120-pair.c
@@ -2,6 +2,10 @@ array_t* all_pairs() {
 	static array_t* all_pairs = NULL;
 	if (!all_pairs) {
 		all_pairs = arr_alloc(1);
+		if (!all_pairs) {
+			throw(error.malloc);
+			return NULL;
+		}
 		arr_push(all_pairs, NULL); // start numbering at 1
 	}
 	return all_pairs;
@@ -9,6 +13,10 @@ array_t* all_pairs() {
 
 pair_t* get_pair(element_t* left, element_t* right) {
 	array_t* pairs = all_pairs();
+	if (!pairs) {
+		throw(error.malloc);
+		return NULL;
+	}
 	for (size_t i = 1; i < pairs->len; ++i) {
 		pair_t* p = (pair_t*) pairs->items[i];
 		if (p->left == left && p->right == right) {
@@ -16,6 +24,10 @@ pair_t* get_pair(element_t* left, element_t* right) {
 		}
 	}
 	pair_t* p = (pair_t*) buffer_alloc(sizeof(pair_t));
+	if (!p) {
+		throw(error.malloc);
+		return NULL;
+	}
 	p->left = left;
 	p->right = right;
 	arr_push(pairs, p);
