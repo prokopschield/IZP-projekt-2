b8e229b25a8b27b2880a9cf3d67a1f41134d2921
Prokop <prokop.schield@protonmail.com>
2021-12-02 19:22:26 +0100

    110-element.c: add malloc tests

--- src/110-element.c
+++ src/110-element.c
@@ -2,6 +2,10 @@ array_t* all_elements() {
 	static array_t* all_elements = NULL;
 	if (!all_elements) {
 		all_elements = arr_alloc(1);
+		if (!all_elements) {
+			throw(error.malloc);
+			return NULL;
+		}
 		arr_push(all_elements, NULL); // start numbering at 1
 	}
 	return all_elements;
@@ -12,6 +16,10 @@ element_t* get_element_by_uid(size_t uid) { return all_elements()->items[uid]; }
 element_t* get_element_by_str(string_t* str, int* index) {
 	string_t el_str = strop_read_word(str, index);
 	array_t* elements = all_elements();
+	if (!el_str.len || !elements) {
+		throw_error("Element allocation failed: '%s'", &str->data[*index]);
+		return NULL;
+	}
 	for (size_t i = 1; i < elements->len; ++i) {
 		element_t* e = elements->items[i];
 		if (str_equal(&el_str, &e->str)) {
@@ -20,6 +28,11 @@ element_t* get_element_by_str(string_t* str, int* index) {
 		}
 	}
 	element_t* new_element = (element_t*) buffer_alloc(sizeof(element_t));
+	if (!new_element) {
+		throw_error("Could not allocate new element '%s'.", el_str.data);
+		str_free(&el_str); // exists, tested previously
+		return NULL;
+	}
 	new_element->uid = elements->len;
 	new_element->str = el_str;
 	arr_push(elements, new_element);
