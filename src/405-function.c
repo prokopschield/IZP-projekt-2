9c4cfcca7a5f2ee26826a01b23d7c9a7f14e0aa0
Prokop <prokop.schield@protonmail.com>
2021-12-03 11:16:53 +0100

    405-function.c: format

--- src/405-function.c
+++ src/405-function.c
@@ -4,14 +4,14 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-bool Cmd_function(rel_t *rel, set_t* universe){
-    for (size_t i = 0; i < rel->number_of_pairs; i++) {
-        for (size_t j = 0; j < universe->number_of_elements; j++) {
-            if (rel->pairs[i]->right != universe->elements[j]) {
-                if (is_pair_in_rel(get_pair(rel->pairs[i]->left, universe->elements[j]), rel))
-                    return false;
-            }
-        }
-    }
-    return true;
+bool Cmd_function(rel_t* rel, set_t* universe) {
+	for (size_t i = 0; i < rel->number_of_pairs; i++) {
+		for (size_t j = 0; j < universe->number_of_elements; j++) {
+			if (rel->pairs[i]->right != universe->elements[j]) {
+				if (is_pair_in_rel(get_pair(rel->pairs[i]->left, universe->elements[j]),  rel))
+					return false;
+			}
+		}
+	}
+	return true;
 }
