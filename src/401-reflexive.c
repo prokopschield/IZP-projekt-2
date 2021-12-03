7b18256b488f204a5209a9f6786699294f2995f0
stefanpeknik <stefa9982@seznam.cz>
2021-12-03 12:41:57 +0100

    401-reflexive.c: rework method based on forum

--- src/401-reflexive.c
+++ src/401-reflexive.c
@@ -2,16 +2,13 @@
 // Created by Štefan Pekník on 22.11.2021.
 //
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-bool Cmd_reflexive (rel_t* rel) {
-    for (size_t i = 0; i < rel->number_of_pairs; i++) {
-        if (!is_pair_in_rel(get_pair(rel->pairs[i]->left, rel->pairs[i]->left), rel))
-            return false;
-        if (!is_pair_in_rel(get_pair(rel->pairs[i]->right, rel->pairs[i]->right), rel))
+bool Cmd_reflexive(set_t* universe, rel_t* rel) {
+    for (size_t i = 0; i < rel->universe->number_of_elements; i++) {
+        if (!is_pair_in_rel(get_pair(rel->elements[i], rel->elements[i]), rel))
             return false;
     }
     return true;
