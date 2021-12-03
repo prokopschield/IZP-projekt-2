105d48f6bf0573715ff3ace732b66e927427b684
Prokop <prokop.schield@protonmail.com>
2021-12-03 11:51:35 +0100

    406-domain.c: format

--- src/406-domain.c
+++ src/406-domain.c
@@ -1,12 +1,12 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-set_t* cmd_domain(rel_t *rel){
-    set_t* setdom = empty_set();
-    for (size_t i = 0; i < rel->number_of_pairs; i++) {
-        if (!is_element_in_set(rel->pairs[i]->left, setdom)){
-            add_element_to_set(rel->pairs[i]->left, &setdom);
-        }
-    }                        
-    return setdom;
+set_t* cmd_domain(rel_t* rel) {
+	set_t* setdom = empty_set();
+	for (size_t i = 0; i < rel->number_of_pairs; i++) {
+		if (!is_element_in_set(rel->pairs[i]->left, setdom)) {
+			add_element_to_set(rel->pairs[i]->left, &setdom);
+		}
+	}
+	return setdom;
 }
