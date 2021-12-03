6f5947f9962929159499b3156c4a270ab053e623
stefanpeknik <stefa9982@seznam.cz>
2021-12-03 12:41:30 +0100

    303-complement.c: edit }

--- src/303-complement.c
+++ src/303-complement.c
@@ -6,7 +6,7 @@
 #include <stdio.h>
 
 set_t* cmd_complement(set_t* universe, set_t* set) {
-    set_t* setc = empty_set();
+    set_t *setc = empty_set();
     for (size_t i = 0; i < universe->number_of_elements; i++) {
         if (!is_element_in_set(universe->elements[i], set)) {
             add_element_to_set(universe->elements[i], &set);
