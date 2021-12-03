87c8bac43ba8dc4ab82d87a387b3030396744bf2
Prokop <prokop.schield@protonmail.com>
2021-12-03 11:16:40 +0100

    404-transitive.c: format

--- src/404-transitive.c
+++ src/404-transitive.c
@@ -6,12 +6,12 @@
 
 bool Cmd_transitive(rel_t* rel) {
 	for (size_t i = 0; i < rel->number_of_pairs; i++) {
-        for (size_t j = 0; j < rel->number_of_pairs; j++) {
-            if (rel->pairs[i]->right == rel->pairs[j]->left) {
-                if (!is_pair_in_rel(get_pair(rel->pairs[i]->left, rel->pairs[j]->right), rel))
-                    return false;
-            }
-        }
+		for (size_t j = 0; j < rel->number_of_pairs; j++) {
+			if (rel->pairs[i]->right == rel->pairs[j]->left) {
+				if (!is_pair_in_rel(get_pair(rel->pairs[i]->left, rel->pairs[j]->right), rel))
+					return false;
+			}
+		}
 	}
 	return true;
 }
