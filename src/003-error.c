a8a97ad4dd5282cf81f7d98b5460286ccf9afdf4
Prokop <prokop.schield@protonmail.com>
2021-12-03 12:16:11 +0100

    720-parse-set.c: migrate error format

--- src/003-error.c
+++ src/003-error.c
@@ -11,3 +11,4 @@ error_t error = { false, false };
 #define throw(e)(e = error.any = true)
 #define throw_error(format, ...)																							 \
 	((error.any = true), fprintf(stderr, "Error :: " format "\n", __VA_ARGS__))
+#define throw_chars(txt) ((error.any = true), fprintf(stderr, "Error :: " txt))
--- src/720-parse-set.c
+++ src/720-parse-set.c
@@ -1,4 +1,4 @@
-set_t* parse_set(array_t* error, set_t* universe, FILE* input, bool EOL) {
+set_t* parse_set(set_t* universe, FILE* input, bool EOL) {
 	set_t* s = empty_set();
 	while (!EOL) {
 		string_t e_str = read_word(input, &EOL);
@@ -6,10 +6,16 @@ set_t* parse_set(array_t* error, set_t* universe, FILE* input, bool EOL) {
 		element_t* e = get_element_by_str(&e_str, &i);
 		str_free(&e_str);
 		if (is_element_in_set(e, s)) {
-			arr_push(error, "Duplicate element in set!");
+			throw_chars("Duplicate element: ");
+			element_print(e, stderr);
+			fprintf(stderr, " is already in ");
+			set_print(s, stderr, false);
 		}
 		if (universe && !is_element_in_set(e, universe)) {
-			arr_push(error, "Element not in universe!");
+			throw_chars("Element ");
+			element_print(e, stderr);
+			fprintf(stderr, " not in Universe: ");
+			set_print(universe, stderr, true);
 		}
 		add_element_to_set(e, &s);
 	}
