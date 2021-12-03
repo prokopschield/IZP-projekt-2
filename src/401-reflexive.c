a4e65d5164a04ee53ff647e11945a9f42b4c68ae
stefanpeknik <stefa9982@seznam.cz>
2021-12-03 12:44:09 +0100

    401-reflexive.c: fix error

--- src/401-reflexive.c
+++ src/401-reflexive.c
@@ -7,8 +7,8 @@
 #include <stdlib.h>
 
 bool Cmd_reflexive(set_t* universe, rel_t* rel) {
-    for (size_t i = 0; i < rel->universe->number_of_elements; i++) {
-        if (!is_pair_in_rel(get_pair(rel->elements[i], rel->elements[i]), rel))
+    for (size_t i = 0; i < universe->number_of_elements; i++) {
+        if (!is_pair_in_rel(get_pair(universe->elements[i], universe->elements[i]), rel))
             return false;
     }
     return true;
