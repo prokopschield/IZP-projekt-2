1bc127207ee376f67015bd791e91ab4736048596
Prokop <prokop.schield@protonmail.com>
2021-12-03 11:16:32 +0100

    403-antisymmetric.c: format

--- src/403-antisymmetric.c
+++ src/403-antisymmetric.c
@@ -5,12 +5,12 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-bool Cmd_antisymmetric(rel_t *rel){
-    for (size_t i = 0; i < rel->number_of_pairs; i++) {
-        if (!(rel->pairs[i]->left == rel->pairs[i]->right)) {
-            if (is_pair_in_rel(get_pair(rel->pairs[i]->right, rel->pairs[i]->left), rel))
-                return false;
-        }
-    }
-    return true;
+bool Cmd_antisymmetric(rel_t* rel) {
+	for (size_t i = 0; i < rel->number_of_pairs; i++) {
+		if (!(rel->pairs[i]->left == rel->pairs[i]->right)) {
+			if (is_pair_in_rel(get_pair(rel->pairs[i]->right, rel->pairs[i]->left),  rel))
+				return false;
+		}
+	}
+	return true;
 }
