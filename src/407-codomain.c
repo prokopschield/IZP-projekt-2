5ee20d83627a3837f4bf448a2186bd432efd690c
PalcivaPapricka <92027322+PalcivaPapricka@users.noreply.github.com>
2021-12-03 11:36:32 +0100

    Update 407-codomain.c

--- src/407-codomain.c
+++ src/407-codomain.c
@@ -4,8 +4,8 @@
 set_t* cmd_codomain(rel_t *rel){
     set_t* setcodom = empty_set();
     for (size_t i = 0; i < rel->number_of_pairs; i++) {
-        if (!is_element_in_set(rel->pairs[i]->left, setcodom)){
-            add_element_to_set(rel->pairs[i]->left, &setcodom);
+        if (!is_element_in_set(rel->pairs[i]->right, setcodom)){
+            add_element_to_set(rel->pairs[i]->right, &setcodom);
         }
     }                        
     return setcodom;
