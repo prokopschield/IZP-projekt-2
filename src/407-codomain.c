9e7cb4c1bc558bfdabe19c2e8f452d640344c9b5
Prokop <prokop.schield@protonmail.com>
2021-12-03 11:51:38 +0100

    407-codomain.c: format

--- src/407-codomain.c
+++ src/407-codomain.c
@@ -1,12 +1,12 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-set_t* cmd_codomain(rel_t *rel){
-    set_t* setcodom = empty_set();
-    for (size_t i = 0; i < rel->number_of_pairs; i++) {
-        if (!is_element_in_set(rel->pairs[i]->right, setcodom)){
-            add_element_to_set(rel->pairs[i]->right, &setcodom);
-        }
-    }                        
-    return setcodom;
+set_t* cmd_codomain(rel_t* rel) {
+	set_t* setcodom = empty_set();
+	for (size_t i = 0; i < rel->number_of_pairs; i++) {
+		if (!is_element_in_set(rel->pairs[i]->right, setcodom)) {
+			add_element_to_set(rel->pairs[i]->right, &setcodom);
+		}
+	}
+	return setcodom;
 }
