ccfb210899b4d0fa5bdb8411d7a8e259c26f317e
Prokop <prokop.schield@protonmail.com>
2021-12-04 11:23:35 +0100

    777-process.c: migrate error format

--- src/777-process.c
+++ src/777-process.c
@@ -6,19 +6,18 @@ int process(FILE* input, FILE* output) {
 	array_t lines = { 0, 0, NULL };
 	line_t line_zero = { 0, line_type_set, empty_set(), NULL, NULL };
 	arr_push(&lines, &line_zero);
-	array_t error = { 0, 0, NULL };
 	size_t line_i = 1;
-	while (!error.len && (!feof(input) || (line_i < lines.len))) {
+	while (!error.any && (!feof(input) || (line_i < lines.len))) {
 		while (line_i >= lines.len) {
 			parse(lines.len > 1 ? (((line_t*) lines.items[1])->val_set) : NULL, &lines, input);
 		}
 		execute(&lines, &line_i, output);
 	}
-	if (error.len) {
-		for (size_t i = 0; i < error.len; ++i) {
-			fprintf(stderr, "Error :: %s\n", (char*) error.items[i]);
-		}
-		fprintf(stderr, "Execution terminated on line %ld.\n", line_i);
+	if (error.malloc) {
+		throw_chars("Memory allocation error.\n");
 	}
-	return error.len;
+	if (error.any) {
+		throw_error("Execution terminated on line %ld.\n", line_i);
+	}
+	return error.any;
 }
