c53370de41cc95d06d2c0a32d94bb56094c88ef7
stefanpeknik <stefa9982@seznam.cz>
2021-12-04 15:25:45 +0100

    501-closure_ref.c: fix rel name sym_rel to ref_rel

--- src/501-closure_ref.c
+++ src/501-closure_ref.c
@@ -6,7 +6,7 @@
 #include <stdio.h>
 
 rel_t* cmd_closure_ref(set_t* universe, rel_t* rel) {
-    rel_t* sym_rel = empty_rel();
+    rel_t* ref_rel = empty_rel();
     for (size_t i = 0; i < universe->number_of_elements; i++) {
         pair_t* pair = get_pair(universe->elements[i], universe->elements[i]);
         if (pair == NULL) {
@@ -14,7 +14,7 @@ rel_t* cmd_closure_ref(set_t* universe, rel_t* rel) {
             return NULL;
         }
         if (!is_pair_in_rel(pair, rel))
-            add_pair_to_rel(pair, &sym_rel);
+            add_pair_to_rel(pair, &ref_rel);
     }
-    return sym_rel;
+    return ref_rel;
 }
