68f0808191ee486a24b16b4a4bfa91ae529c25a8
Prokop <prokop.schield@protonmail.com>
2021-12-04 17:25:21 +0100

    770-execute.c: fix mistake

--- src/770-execute.c
+++ src/770-execute.c
@@ -7,7 +7,7 @@ void execute(array_t* lines, size_t* line_i, FILE* output) {
 	evaled_t evaled = eval(lines, line);
 	if (evaled.s) {
 		fprintf(output, "%s\n", evaled.s);
-	} else if (evaled.n) {
+	} else if (evaled.u) { // if use flag is set
 		fprintf(output, "%lld\n", evaled.n);
 	} else if (line->val_set) {
 		set_print(line->val_set, output, *line_i == 1);
