dd954f573d89d5b1048a8bcbdd19f409cb98417e
Prokop <prokop.schield@protonmail.com>
2021-12-04 16:29:52 +0100

    770-execute.c: call eval()

--- src/770-execute.c
+++ src/770-execute.c
@@ -1,7 +1,13 @@
 void execute(array_t* lines, size_t* line_i, FILE* output) {
-	line_t* line = lines->items[*line_i];
-	// const bool bval = line->val_cmd && eval(error, lines->items[1], line);
-	if (line->val_set) {
+	line_t* line = line_get(lines, *line_i);
+	if (!line) {
+		throw_error("Line %ld not loadable for some reason!", *line_i);
+		return;
+	}
+	char* txtval = eval(lines, line);
+	if (txtval) {
+		fprintf(output, "%s\n", txtval);
+	} else if (line->val_set) {
 		set_print(line->val_set, output, *line_i == 1);
 	} else if (line->val_rel) {
 		rel_print(line->val_rel, output, 0);
