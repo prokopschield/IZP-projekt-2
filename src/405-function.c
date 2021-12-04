8aa357c00307f6628ad090162195114c0bfa855e
Prokop <prokop.schield@protonmail.com>
2021-12-04 10:44:48 +0100

    405-function.c: format

--- src/405-function.c
+++ src/405-function.c
@@ -8,11 +8,11 @@ bool cmd_function(set_t* universe, rel_t* rel) {
 	for (size_t i = 0; i < rel->number_of_pairs; i++) {
 		for (size_t j = 0; j < universe->number_of_elements; j++) {
 			if (rel->pairs[i]->right != universe->elements[j]) {
-                pair_t* pair = get_pair(rel->pairs[i]->left, universe->elements[j]);
-                if (pair == NULL) {
-                    throw_chars("could not allocate memory for pair\n");
-                    return false;
-                }
+				pair_t* pair = get_pair(rel->pairs[i]->left, universe->elements[j]);
+				if (pair == NULL) {
+					throw_chars("could not allocate memory for pair\n");
+					return false;
+				}
 				if (is_pair_in_rel(pair, rel))
 					return false;
 			}
