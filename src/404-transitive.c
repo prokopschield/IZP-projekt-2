f3d786726860c1cedd99d87084d3506f5ddb0127
Prokop <prokop.schield@protonmail.com>
2021-12-04 10:44:42 +0100

    404-transitive.c: format

--- src/404-transitive.c
+++ src/404-transitive.c
@@ -8,11 +8,11 @@ bool cmd_transitive(rel_t* rel) {
 	for (size_t i = 0; i < rel->number_of_pairs; i++) {
 		for (size_t j = 0; j < rel->number_of_pairs; j++) {
 			if (rel->pairs[i]->right == rel->pairs[j]->left) {
-                pair_t* pair = get_pair(rel->pairs[i]->left, rel->pairs[j]->right);
-                if (pair == NULL) {
-                    throw_chars("could not allocate memory for pair\n");
-                    return false;
-                }
+				pair_t* pair = get_pair(rel->pairs[i]->left, rel->pairs[j]->right);
+				if (pair == NULL) {
+					throw_chars("could not allocate memory for pair\n");
+					return false;
+				}
 				if (!is_pair_in_rel(pair, rel))
 					return false;
 			}
