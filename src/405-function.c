0d19d59b50ce3b35acf73cda58fa0a3616e91f47
stefanpeknik <stefa9982@seznam.cz>
2021-12-04 10:27:24 +0100

    405-function.c: add error handling for get_pair

--- src/405-function.c
+++ src/405-function.c
@@ -4,11 +4,16 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-bool Cmd_function(rel_t* rel, set_t* universe) {
+bool cmd_function(set_t* universe, rel_t* rel) {
 	for (size_t i = 0; i < rel->number_of_pairs; i++) {
 		for (size_t j = 0; j < universe->number_of_elements; j++) {
 			if (rel->pairs[i]->right != universe->elements[j]) {
-				if (is_pair_in_rel(get_pair(rel->pairs[i]->left, universe->elements[j]),  rel))
+                pair_t* pair = get_pair(rel->pairs[i]->left, universe->elements[j]);
+                if (pair == NULL) {
+                    throw_chars("could not allocate memory for pair\n");
+                    return false;
+                }
+				if (is_pair_in_rel(pair, rel))
 					return false;
 			}
 		}
