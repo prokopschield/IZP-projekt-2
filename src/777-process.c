c91a84e3d026cd55de7b7326e972aab67fdd72c4
Prokop <prokop.schield@protonmail.com>
2021-12-04 19:41:07 +0100

    777-process.c: load everything first

--- src/777-process.c
+++ src/777-process.c
@@ -4,13 +4,11 @@ int process(FILE* input, FILE* output) {
 	}
 	array_t lines = line_array_init();
 	size_t line_i = 1;
-	while (!error.any && (!feof(input) || (line_i < lines.len))) {
-		while (!error.any && (line_i >= lines.len)) {
-			parse(lines.len > 1 ? (((line_t*) lines.items[1])->val_set) : NULL, &lines, input);
-		}
-		if (!error.any) {
-			execute(&lines, &line_i, output);
-		}
+	while (!error.any && !feof(input)) {
+		parse(lines.len > 1 ? (((line_t*) lines.items[1])->val_set) : NULL, &lines, input);
+	}
+	while (!error.any && (line_i < lines.len)) {
+		execute(&lines, &line_i, output);
 	}
 	if (error.malloc) {
 		throw_chars("Memory allocation error.\n");
