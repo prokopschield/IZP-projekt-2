fab526b30f6c07d9a4d3a359e3f95951cba1520f
Prokop <prokop.schield@protonmail.com>
2021-12-04 10:44:30 +0100

    402-symmetric.c: format

--- src/402-symmetric.c
+++ src/402-symmetric.c
@@ -1,19 +1,19 @@
 //
 // Created by jiris on 30/11/2021.
 //
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
-bool cmd_symmetric(rel_t* rel) {
-    for (size_t i = 0; i < rel->number_of_pairs; i++) {
-        pair_t* pair = get_pair(rel->pairs[i]->right, rel->pairs[i]->left);
-        if (pair == NULL) {
-            throw_chars("could not allocate memory for pair\n");
-            return false;
-        }
-        if (!is_pair_in_rel(pair, rel))
-            return false;
-    }
-    return true;
+bool cmd_symmetric(rel_t* rel) { 
+	for (size_t i = 0; i < rel->number_of_pairs; i++) { 
+		pair_t* pair = get_pair(rel->pairs[i]->right, rel->pairs[i]->left);
+		if (pair == NULL) { 
+			throw_chars("could not allocate memory for pair\n");
+			return false;
+		}
+		if (!is_pair_in_rel(pair, rel))
+			return false;
+	}
+	return true;
 }
