c5c1826a20fe7d1151aa43fc6719aa203ab3f2e0
stefanpeknik <stefa9982@seznam.cz>
2021-12-04 15:25:50 +0100

    502-closure_sym.c: finish method

--- src/502-closure_sym.c
+++ src/502-closure_sym.c
@@ -2,3 +2,21 @@
 // Created by Štefan Pekník on 04.12.2021.
 //
 
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+rel_t* cmd_closure_sym(rel_t* rel) {
+    rel_t* sym_rel = empty_rel();
+    for (size_t i = 0; i < rel->number_of_pairs; i++) {
+        pair_t *pair = get_pair(rel->pairs[i]->right, rel->pairs[i]->left);
+        if (pair == NULL) {
+            throw_chars("could not allocate memory for pair\n");
+            return false;
+        }
+        if (!is_pair_in_rel(pair, rel))
+            add_pair_to_rel(pair, &sym_rel);
+    }
+    return sym_rel;
+}
