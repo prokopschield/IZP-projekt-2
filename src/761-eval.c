08682cdc7ba84f0dea22c81aacbe901a67a108e5
Prokop <prokop.schield@protonmail.com>
2021-12-05 01:00:33 +0100

    761-eval.c: remove argument array length check

--- src/761-eval.c
+++ src/761-eval.c
@@ -7,31 +7,29 @@ evaled_t eval(array_t* lines, line_t* line) {
 		cmd_t* cmd = line->val_cmd;
 		char* cmd_s = (char*) cmd->cmd.data;
 		size_t_array_t* args = (size_t_array_t*) cmd->args;
-		if (args->len) {
-			if (!strcmp(cmd_s, "empty")) {
-				arg_A_t a = { NULL, 0 };
-				if (arg_A_val(&a, lines, args)) {
-					if (cmd_empty(a.A)) {
-						ret.s = "true";
-					} else {
-						ret.s = "false";
-						ret.N = a.N;
-					}
-					return ret;
+		if (!strcmp(cmd_s, "empty")) {
+			arg_A_t a = { NULL, 0 };
+			if (arg_A_val(&a, lines, args)) {
+				if (cmd_empty(a.A)) {
+					ret.s = "true";
+				} else {
+					ret.s = "false";
+					ret.N = a.N;
 				}
-			} else if (!strcmp(cmd_s, "card")) {
-				arg_A_t a = { NULL, 0 };
-				if (arg_A_val(&a, lines, args)) {
-					ret.u = true;
-					ret.n = (long long int) cmd_card(a.A);
-					return ret;
-				}
-			} else if (!strcmp(cmd_s, "complement")) {
-				arg_A_t a = { NULL, 0 };
-				if (arg_A_val(&a, lines, args)) {
-					line->val_set = cmd_complement(get_universe(lines), a.A);
-					return ret;
-				}
+				return ret;
+			}
+		} else if (!strcmp(cmd_s, "card")) {
+			arg_A_t a = { NULL, 0 };
+			if (arg_A_val(&a, lines, args)) {
+				ret.u = true;
+				ret.n = (long long int) cmd_card(a.A);
+				return ret;
+			}
+		} else if (!strcmp(cmd_s, "complement")) {
+			arg_A_t a = { NULL, 0 };
+			if (arg_A_val(&a, lines, args)) {
+				line->val_set = cmd_complement(get_universe(lines), a.A);
+				return ret;
 			}
 		}
 	}
