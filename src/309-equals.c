353de332640a3ab04c7be859275fe66890b4a6e3
stefanpeknik <stefa9982@seznam.cz>
2021-12-05 18:09:15 +0100

    309-equals.c: rename: set1 -> set_A, set2 -> set_B

--- src/309-equals.c
+++ src/309-equals.c
@@ -5,8 +5,8 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-bool cmd_equals(set_t* set1, set_t* set2) {
-    if (set1 == set2)
+bool cmd_equals(set_t* set_A, set_t* set_B) {
+    if (set_A == set_B)
         return true;
     else
         return false;
