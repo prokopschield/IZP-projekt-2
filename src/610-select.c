c416fa54583d8732e9ca86ae141456bde2a3f8e5
Prokop <prokop.schield@protonmail.com>
2021-12-05 21:12:31 +0100

    610-select.c: format

--- src/610-select.c
+++ src/610-select.c
@@ -4,9 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 set_t* cmd_select(set_t* set) {
-    set_t* newset = empty_set();
-    add_element_to_set(set->elements[rand() % set->number_of_elements], &newset);
-    return newset;
+	set_t* newset = empty_set();
+	add_element_to_set(set->elements[rand() % set->number_of_elements], &newset);
+	return newset;
 }
