f32556fef2c1fef89e6390dfec83fd87eca4274a
stefanpeknik <stefa9982@seznam.cz>
2021-12-05 17:30:29 +0100

    301-empty.c: add comm

--- src/301-empty.c
+++ src/301-empty.c
@@ -1,9 +1,7 @@
-//
-// Created by Štefan Pekník on 15.11.2021.
-//
 #include <stdbool.h>
 #include <stdio.h>
 
+// returns true if empty, false if not empty
 bool cmd_empty(set_t* set) {
     if (set->number_of_elements == 0)
         return true;
