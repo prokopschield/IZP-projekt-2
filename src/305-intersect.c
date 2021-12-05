753ef8ef681372bd108fd2de91aa8e0ce014b26d
stefanpeknik <stefa9982@seznam.cz>
2021-12-05 18:01:18 +0100

    305-intersect.c: rename: set1 -> set_A, set2 -> set_B

--- src/305-intersect.c
+++ src/305-intersect.c
@@ -1,12 +1,12 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-// checks whether each element from set1 is in set2 as well, if it is, it is added to new set inter_set
-set_t* cmd_intersect(set_t* set1, set_t* set2) {
+// checks whether each element from set1 is in set_B as well, if it is, it is added to new set inter_set
+set_t* cmd_intersect(set_t* set_A, set_t* set_B) {
     set_t *inter_set = empty_set();
-    for (size_t i = 0; i < set1->number_of_elements; i++) {
-        if (is_element_in_set(set1->elements[i], set2)) {
-            add_element_to_set(set1->elements[i], &inter_set);
+    for (size_t i = 0; i < set_A->number_of_elements; i++) {
+        if (is_element_in_set(set_A->elements[i], set_B)) {
+            add_element_to_set(set_A->elements[i], &inter_set);
         }
     }
     return inter_set;
