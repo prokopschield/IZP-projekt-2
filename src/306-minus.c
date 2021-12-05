741fdb1ca54f8cbb57a7a09f11080c7c6d252dab
stefanpeknik <stefa9982@seznam.cz>
2021-12-05 17:46:11 +0100

    306-minus.c: rename set name + remove useless comms + add comm

--- src/306-minus.c
+++ src/306-minus.c
@@ -1,15 +1,12 @@
-//
-// Created by Štefan Pekník on 19.11.2021.
-//
-
 #include <stdbool.h>
 #include <stdio.h>
 
+// checks if each element from set1 is not as well in set2, if not, it is added to new set min_set
 set_t* cmd_minus(set_t* set1, set_t* set2) {
-    set_t *setm = empty_set();
+    set_t *min_set = empty_set();
     for (size_t i = 0; i < set1->number_of_elements; i++) {
         if (!is_element_in_set(set1->elements[i], set2))
-            add_element_to_set(set1->elements[i], &setm);
+            add_element_to_set(set1->elements[i], &min_set);
     }
-    return setm;
+    return min_set;
 }
