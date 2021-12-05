3e6fd285a3f6d1d4c4f85f9ee1df7c523663eabf
stefanpeknik <stefa9982@seznam.cz>
2021-12-05 19:31:00 +0100

    403-antisymmetric.c:  add comms

--- src/403-antisymmetric.c
+++ src/403-antisymmetric.c
@@ -1,6 +1,9 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+// checks if every pair in the rel does not have its symmetric pair in the rel (e.g. (a, b) !-> (b, a)),
+// if any pair has its symmetric pair, returns false,
+// else returns true
 bool cmd_antisymmetric(rel_t* rel) {
 	for (size_t i = 0; i < rel->number_of_pairs; i++) {
 		if (!(rel->pairs[i]->left == rel->pairs[i]->right)) {
