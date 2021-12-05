736b6c7e12674e5ac7ad65123ba4a0fcbb7833e9
stefanpeknik <stefa9982@seznam.cz>
2021-12-05 18:08:53 +0100

    307-subseteq.c: rename: set1 -> set_A, set2 -> set_B

--- src/307-subseteq.c
+++ src/307-subseteq.c
@@ -1,13 +1,10 @@
-//
-// Created by Štefan Pekník on 19.11.2021.
-//
-
 #include <stdbool.h>
 #include <stdio.h>
 
-bool cmd_subseteq(set_t* set1, set_t* set2) {
-    for (size_t i = 0; i < set2->number_of_elements; ++i) {
-        if (!is_element_in_set(set2->elements[i], set1))
+//
+bool cmd_subseteq(set_t* set_A, set_t* set_B) {
+    for (size_t i = 0; i < set_A->number_of_elements; i++) {
+        if (!is_element_in_set(set_A->elements[i], set_B))
             return false;
     }
     return true;
