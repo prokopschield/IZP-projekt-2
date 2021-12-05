6233e47b7cb43726d40920f874167ebb856851c8
stefanpeknik <stefa9982@seznam.cz>
2021-12-05 19:30:57 +0100

    401-reflexive.c: add comms

--- src/401-reflexive.c
+++ src/401-reflexive.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+// checks if every element in the universe has its own refeflexive pair (e.g. element a : pair (a, a)) included in the rel
 bool cmd_reflexive(set_t* universe, rel_t* rel) {
     for (size_t i = 0; i < universe->number_of_elements; i++) {
         pair_t* pair = get_pair(universe->elements[i], universe->elements[i]);
