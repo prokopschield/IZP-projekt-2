880fe9597020687589e8cb153fe0da62f05bc941
stefanpeknik <stefa9982@seznam.cz>
2021-12-05 19:31:02 +0100

    405-function.c:  add comms

--- src/405-function.c
+++ src/405-function.c
@@ -1,6 +1,8 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+// takes a left element from a pair
+// and checks whether there is not any other pair containing the left element from the pair with different right element
 bool cmd_function(set_t* universe, rel_t* rel) {
 	for (size_t i = 0; i < rel->number_of_pairs; i++) {
 		for (size_t j = 0; j < universe->number_of_elements; j++) {
