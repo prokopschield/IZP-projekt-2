0c6486077ae3c1cc7ad5218c3e1a08b87e3d1ce5
stefanpeknik <stefa9982@seznam.cz>
2021-12-05 17:36:58 +0100

    305-intersect.c:  add comm + rename set variable

--- src/305-intersect.c
+++ src/305-intersect.c
@@ -5,12 +5,13 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+// checks whether each element from set1 is in set2 as well, if it is, it is added to new set inter_set
 set_t* cmd_intersect(set_t* set1, set_t* set2) {
-    set_t *seti = empty_set();
+    set_t *inter_set = empty_set();
     for (size_t i = 0; i < set1->number_of_elements; i++) {
         if (is_element_in_set(set1->elements[i], set2)) {
-            add_element_to_set(set1->elements[i], &seti);
+            add_element_to_set(set1->elements[i], &inter_set);
         }
     }
-    return seti;
+    return inter_set;
 }
