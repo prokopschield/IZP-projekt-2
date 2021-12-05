f1ce28eaf62b738ae44a3dc23a1840149cabd170
Prokop <prokop.schield@protonmail.com>
2021-12-05 18:34:39 +0100

    308-subset.c: format

--- src/308-subset.c
+++ src/308-subset.c
@@ -2,10 +2,12 @@
 #include <stdio.h>
 
 // checks whether set_A is subseteq of set_B and
-// if number of elements in set_A is lower than in set_B return true, else returns false
+// if number of elements in set_A is lower than in set_B return true, else
+// returns false
 bool cmd_subset(set_t* set_A, set_t* set_B) {
-    if (cmd_subseteq(set_A, set_B) && set_A->number_of_elements < set_B->number_of_elements)
-        return true;
-    else
-        return false;
+	if (cmd_subseteq(set_A, set_B) &&
+			set_A->number_of_elements < set_B->number_of_elements)
+		return true;
+	else
+		return false;
 }
