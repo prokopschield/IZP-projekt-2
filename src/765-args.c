b0b7402b2549d9ef5ced9756959c309b5dfeaa02
Prokop <prokop.schield@protonmail.com>
2021-12-05 12:28:15 +0100

    765-args.c: add arg_AB_val()

--- src/765-args.c
+++ src/765-args.c
@@ -1,4 +1,4 @@
-bool arg_A_val(arg_A_t* res, array_t* lines, size_t_array_t* args, size_t max_args) {
+bool arg_A_val(arg_A_t* res, array_t* lines, size_t_array_t* args,  size_t max_args) {
 	if (args->len < 1) {
 		throw_chars("Insufficient arguments: ");
 		return false;
@@ -17,4 +17,26 @@ bool arg_A_val(arg_A_t* res, array_t* lines, size_t_array_t* args, size_t max_ar
 		invalid_argument(arg_1);
 		return false;
 	}
+}
+
+bool arg_AB_val(arg_AB_t* res, array_t* lines, size_t_array_t* args, size_t max_args) {
+	if (args->len < 2) {
+		throw_chars("Insufficient arguments: ");
+		return false;
+	}
+	line_t* arg_1 = lineload(lines, args->items[0]);
+	if (!(res->A = arg_1->val_set)) {
+		invalid_argument(arg_1);
+	}
+	line_t* arg_2 = lineload(lines, args->items[0]);
+	if (!(res->B = arg_2->val_set)) {
+		invalid_argument(arg_2);
+	}
+	if (args->len >= 2) {
+		res->N = args->items[1];
+	}
+	if (args->len > max_args) {
+		throw_error("Too many arguments: %ld > %ld", args->len, max_args);
+	}
+	return (res->A && res->B);
 }
