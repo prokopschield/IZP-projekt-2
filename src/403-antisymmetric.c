dc6636331cd38afd7989b9929c883a8ce3012481
stefanpeknik <stefa9982@seznam.cz>
2021-12-05 19:41:15 +0100

    403-antisymmetric.c: edit to be more readable

--- src/403-antisymmetric.c
+++ src/403-antisymmetric.c
@@ -6,7 +6,7 @@
 // else returns true
 bool cmd_antisymmetric(rel_t* rel) {
 	for (size_t i = 0; i < rel->number_of_pairs; i++) {
-		if (!(rel->pairs[i]->left == rel->pairs[i]->right)) {
+		if (rel->pairs[i]->left != rel->pairs[i]->right) {
             pair_t* pair = get_pair(rel->pairs[i]->right, rel->pairs[i]->left);
             if (pair == NULL) {
                 throw_chars("could not allocate memory for pair\n");
