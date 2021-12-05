80276351fc95797a3566c66b524ee497414526a0
stefanpeknik <stefa9982@seznam.cz>
2021-12-05 18:30:25 +0100

    308-subset.c: fix bug

--- src/308-subset.c
+++ src/308-subset.c
@@ -1,14 +1,10 @@
-//
-// Created by Štefan Pekník on 19.11.2021.
-//
-
 #include <stdbool.h>
 #include <stdio.h>
 
+// checks whether set_A is subseteq of set_B and
+// if number of elements in set_A is lower than in set_B return true, else returns false
 bool cmd_subset(set_t* set_A, set_t* set_B) {
-    if (set_A == set_B)
-        return false;
-    else if (cmd_subseteq(set_A, set_B))
+    if (cmd_subseteq(set_A, set_B) && set_A->number_of_elements < set_B->number_of_elements)
         return true;
     else
         return false;
