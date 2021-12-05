cb6a735cdeffe380045c976f265a0e3dfb7bade0
Prokop <prokop.schield@protonmail.com>
2021-12-05 18:34:33 +0100

    305-intersect.c: format

--- src/305-intersect.c
+++ src/305-intersect.c
@@ -1,13 +1,14 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-// checks whether each element from set1 is in set_B as well, if it is, it is added to new set inter_set
+// checks whether each element from set1 is in set_B as well, if it is, it is
+// added to new set inter_set
 set_t* cmd_intersect(set_t* set_A, set_t* set_B) {
-    set_t *inter_set = empty_set();
-    for (size_t i = 0; i < set_A->number_of_elements; i++) {
-        if (is_element_in_set(set_A->elements[i], set_B)) {
-            add_element_to_set(set_A->elements[i], &inter_set);
-        }
-    }
-    return inter_set;
+	set_t* inter_set = empty_set();
+	for (size_t i = 0; i < set_A->number_of_elements; i++) {
+		if (is_element_in_set(set_A->elements[i], set_B)) {
+			add_element_to_set(set_A->elements[i], &inter_set);
+		}
+	}
+	return inter_set;
 }
