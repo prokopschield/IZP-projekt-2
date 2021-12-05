c2525e47ca2decd55e4b0ba55c1a4bf70e4d412b
stefanpeknik <stefa9982@seznam.cz>
2021-12-05 17:30:48 +0100

    303-complement.c: add comm + edit variable set name

--- src/303-complement.c
+++ src/303-complement.c
@@ -5,12 +5,13 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+//goes through universe and checks whether each element in set is in universe, if not, it is added to new set comp_set
 set_t* cmd_complement(set_t* universe, set_t* set) {
-    set_t *setc = empty_set();
+    set_t *comp_set = empty_set();
     for (size_t i = 0; i < universe->number_of_elements; i++) {
         if (!is_element_in_set(universe->elements[i], set)) {
-            add_element_to_set(universe->elements[i], &setc);
+            add_element_to_set(universe->elements[i], &comp_set);
         }
     }
-    return setc;
+    return comp_set;
 }
