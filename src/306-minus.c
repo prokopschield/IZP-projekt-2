9a64b8e607f1949e4c7db4784598112bd35a366a
Prokop <prokop.schield@protonmail.com>
2021-12-05 18:34:34 +0100

    306-minus.c: format

--- src/306-minus.c
+++ src/306-minus.c
@@ -1,12 +1,13 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-// checks if each element from set_A is not as well in set_B, if not, it is added to new set min_set
+// checks if each element from set_A is not as well in set_B, if not, it is
+// added to new set min_set
 set_t* cmd_minus(set_t* set_A, set_t* set_B) {
-    set_t *min_set = empty_set();
-    for (size_t i = 0; i < set_A->number_of_elements; i++) {
-        if (!is_element_in_set(set_A->elements[i], set_B))
-            add_element_to_set(set_A->elements[i], &min_set);
-    }
-    return min_set;
+	set_t* min_set = empty_set();
+	for (size_t i = 0; i < set_A->number_of_elements; i++) {
+		if (!is_element_in_set(set_A->elements[i], set_B))
+			add_element_to_set(set_A->elements[i], &min_set);
+	}
+	return min_set;
 }
