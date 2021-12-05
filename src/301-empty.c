79149ed9ad9b8613133d1fc85cdb7e33aaf8602e
Prokop <prokop.schield@protonmail.com>
2021-12-05 18:34:22 +0100

    301-empty.c: format

--- src/301-empty.c
+++ src/301-empty.c
@@ -3,7 +3,7 @@
 
 // returns true if empty, false if not empty
 bool cmd_empty(set_t* set) {
-    if (set->number_of_elements == 0)
-        return true;
-    return false;
+	if (set->number_of_elements == 0)
+		return true;
+	return false;
 }
