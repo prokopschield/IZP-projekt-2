f578e555c8077c00f80cddf0d82ed37e2b6fbb2d
Prokop <prokop.schield@protonmail.com>
2021-12-05 22:46:00 +0100

    4xx: rewrite *jective

--- src/408-injective.c
+++ src/408-injective.c
@@ -2,26 +2,17 @@
 #include <stdio.h>
 
 bool cmd_injective(rel_t* rel, set_t* set1, set_t* set2) {
-	set_t* setright = empty_set();
-	set_t* setleft = empty_set();
-	for (size_t i = 0; i < rel->number_of_pairs; i++) {
-		if (!is_element_in_set(rel->pairs[i]->right, setright) &&
-				is_element_in_set(rel->pairs[i]->right, set2)) {
-			add_element_to_set(rel->pairs[i]->right, &setright);
-		} else {
-			return false;
-		}
-		if (!is_element_in_set(rel->pairs[i]->left, setleft) &&
-				is_element_in_set(rel->pairs[i]->right, set1)) {
-			add_element_to_set(rel->pairs[i]->left, &setleft);
-		} else {
-			return false;
-		}
+	if (rel == empty_rel()) {
+		return true;
 	}
-
-	if (set1->number_of_elements != setleft->number_of_elements) {
+	if (!cmd_subseteq(cmd_domain(rel), set1)) {
 		return false;
 	}
-
-	return true;
+	if (!cmd_subseteq(cmd_codomain(rel), set2)) {
+		return false;
+	}
+	if (!cmd_function(rel)) {
+		return false;
+	}
+	return (set1->number_of_elements == rel->number_of_pairs);
 }
--- src/409-surjective.c
+++ src/409-surjective.c
@@ -2,29 +2,14 @@
 #include <stdio.h>
 
 bool cmd_surjective(rel_t* rel, set_t* set1, set_t* set2) {
-	set_t* setright = empty_set();
-	set_t* setleft = empty_set();
-	for (size_t i = 0; i < rel->number_of_pairs; i++) {
-		if (!is_element_in_set(rel->pairs[i]->right, setright)) {
-			add_element_to_set(rel->pairs[i]->right, &setright);
-		}
-
-		if (!is_element_in_set(rel->pairs[i]->right, set2)) {
-			return false;
-		}
-
-		if (!is_element_in_set(rel->pairs[i]->left, setleft) &&
-				is_element_in_set(rel->pairs[i]->right, set1)) {
-			add_element_to_set(rel->pairs[i]->left, &setleft);
-		} else {
-			return false;
-		}
+	if (rel == empty_rel()) {
+		return true;
 	}
-
-	if (set1->number_of_elements != setleft->number_of_elements ||
-			set2->number_of_elements != setright->number_of_elements) {
+	if (!cmd_subseteq(cmd_domain(rel), set1)) {
 		return false;
 	}
-
-	return true;
+	if (!cmd_function(rel)) {
+		return false;
+	}
+	return (cmd_codomain(rel) == set2);
 }
