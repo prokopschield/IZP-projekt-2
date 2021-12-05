8581958b2d0ae66ba6a51d623186ef0b653248dd
stefanpeknik <stefa9982@seznam.cz>
2021-12-05 17:30:33 +0100

    302-card.c: add comm

--- src/302-card.c
+++ src/302-card.c
@@ -1,8 +1,5 @@
-//
-// Created by jiris on 15/11/2021.
-//
-
 #include <stdbool.h>
 #include <stdio.h>
 
+//returns number of elements in the given set
 size_t cmd_card(set_t* set) { return set->number_of_elements; }
