9ae0461cc7d056b70f55a97d9565bcea73e727fc
Prokop <prokop.schield@protonmail.com>
2021-12-05 18:34:36 +0100

    307-subseteq.c: format

--- src/307-subseteq.c
+++ src/307-subseteq.c
@@ -3,9 +3,9 @@
 
 // checks whether every element from set_A is in set_B as well
 bool cmd_subseteq(set_t* set_A, set_t* set_B) {
-    for (size_t i = 0; i < set_A->number_of_elements; i++) {
-        if (!is_element_in_set(set_A->elements[i], set_B))
-            return false;
-    }
-    return true;
+	for (size_t i = 0; i < set_A->number_of_elements; i++) {
+		if (!is_element_in_set(set_A->elements[i], set_B))
+			return false;
+	}
+	return true;
 }
