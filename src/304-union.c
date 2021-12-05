89b88170e9212c92178b79d90c82dc506eb355c1
stefanpeknik <stefa9982@seznam.cz>
2021-12-05 18:01:13 +0100

    304-union.c: rename: set1 -> set_A, set2 -> set_B

--- src/304-union.c
+++ src/304-union.c
@@ -1,14 +1,14 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-// creates new set uni_set which contains all elements from set1,
-// goes through all elements in set2 and
+// creates new set uni_set which contains all elements from set_A,
+// goes through all elements in set_B and
 // adds every new element (element which is not in set uni_set) to set uni_set
-set_t* cmd_union(set_t* set1, set_t* set2) {
-    set_t *uni_set = set1;
-    for (size_t i = 0; i < set2->number_of_elements; i++) {
-        if (!is_element_in_set(set2->elements[i], uni_set))
-            add_element_to_set(set2->elements[i], &uni_set);
+set_t* cmd_union(set_t* set_A, set_t* set_B) {
+    set_t *uni_set = set_A;
+    for (size_t i = 0; i < set_B->number_of_elements; i++) {
+        if (!is_element_in_set(set_B->elements[i], uni_set))
+            add_element_to_set(set_B->elements[i], &uni_set);
     }
     return uni_set;
 }
