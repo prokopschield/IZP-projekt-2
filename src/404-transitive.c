e53a77e72464e9a4ac6189c16484f1f617cd91ca
stefanpeknik <stefa9982@seznam.cz>
2021-12-05 19:31:01 +0100

    404-transitive.c:  add comms

--- src/404-transitive.c
+++ src/404-transitive.c
@@ -1,6 +1,8 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+// takes pair and finds other pair by rule: a right element from pair1 equals a left element from pairN;
+// and checks whether there is also a pair made of a left element from pair1 and a right element from pairN
 bool cmd_transitive(rel_t* rel) {
 	for (size_t i = 0; i < rel->number_of_pairs; i++) {
 		for (size_t j = 0; j < rel->number_of_pairs; j++) {
