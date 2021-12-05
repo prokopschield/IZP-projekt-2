4277e78c4c47199764019c22d16b8e526d2904c2
Prokop <prokop.schield@protonmail.com>
2021-12-05 22:52:57 +0100

    308-subset.c: format

--- src/308-subset.c
+++ src/308-subset.c
@@ -5,9 +5,9 @@
 // if number of elements in set_A is lower than in set_B return true, else
 // returns false
 bool cmd_subset(set_t* set_A, set_t* set_B) {
-	if (cmd_subseteq(set_A, set_B) &&
-			set_A->number_of_elements < set_B->number_of_elements)
+	if (cmd_subseteq(set_A, set_B) && set_A->number_of_elements < set_B->number_of_elements) {
 		return true;
-	else
+	} else {
 		return false;
+	}
 }
