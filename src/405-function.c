feb731b930f9bb87ecb8b8889c5d52a1385d7b43
Prokop <prokop.schield@protonmail.com>
2021-12-05 19:59:17 +0100

    405-function.c: improve logic

--- src/405-function.c
+++ src/405-function.c
@@ -1,20 +1,12 @@
 #include <stdbool.h>
-#include <stdio.h>
 
-// takes a left element from a pair
-// and checks whether there is not any other pair containing the left element from the pair with different right element
-bool cmd_function(set_t* universe, rel_t* rel) {
-	for (size_t i = 0; i < rel->number_of_pairs; i++) {
-		for (size_t j = 0; j < universe->number_of_elements; j++) {
-			if (rel->pairs[i]->right != universe->elements[j]) {
-				pair_t* pair = get_pair(rel->pairs[i]->left, universe->elements[j]);
-				if (pair == NULL) {
-					throw_chars("could not allocate memory for pair\n");
-					return false;
-				}
-				if (is_pair_in_rel(pair, rel))
-					return false;
-			}
+bool cmd_function(rel_t* rel) {
+	set_t* domain = empty_set();
+	for (size_t i = 0; i < rel->number_of_pairs; ++i) {
+		if (is_element_in_set(rel->pairs[i]->left, domain)) {
+			return false;
+		} else {
+			add_element_to_set(rel->pairs[i]->left, &domain);
 		}
 	}
 	return true;
