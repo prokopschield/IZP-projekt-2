a9b4887637a5e1ffe1b8717d20331c225fa63d32
stefanpeknik <stefa9982@seznam.cz>
2021-12-05 17:36:45 +0100

    303-complement.c: add comm + rename set variable

--- src/303-complement.c
+++ src/303-complement.c
@@ -5,7 +5,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-//goes through universe and checks whether each element in set is in universe, if not, it is added to new set comp_set
+// goes through universe and checks whether each element in set is in universe, if not, it is added to new set comp_set
 set_t* cmd_complement(set_t* universe, set_t* set) {
     set_t *comp_set = empty_set();
     for (size_t i = 0; i < universe->number_of_elements; i++) {
