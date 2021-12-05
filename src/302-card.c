e74975b66447bfcb133f82dd87c85b0bf8bf4322
stefanpeknik <stefa9982@seznam.cz>
2021-12-05 17:36:34 +0100

    302-card.c: add comm

--- src/302-card.c
+++ src/302-card.c
@@ -1,5 +1,5 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-//returns number of elements in the given set
+// returns number of elements in the given set
 size_t cmd_card(set_t* set) { return set->number_of_elements; }
