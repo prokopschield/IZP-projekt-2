51d544723250b0521f433fe4d2fcab893a6be649
Jurgee <jiristipek@email.cz>
2021-12-05 16:33:30 +0100

    Finish method

---
+++ src/610-select.c
@@ -0,0 +1,12 @@
+//
+// Created by jiris on 04/12/2021.
+//
+#include <stdio.h>
+#include <stdlib.h>
+
+
+set_t* cmd_select(set_t* set) {
+    set_t* newset = empty_set();
+    add_element_to_set(set->elements[rand() % set->number_of_elements], &newset);
+    return newset;
+}
