b22eb4a18dc3ebc78555615568b386a295658647
Prokop <prokop.schield@protonmail.com>
2021-12-05 20:20:05 +0100

    765-args.c: do not coerse rel to set

--- src/765-args.c
+++ src/765-args.c
@@ -4,19 +4,19 @@ bool arg_A_val(arg_A_t* res, array_t* lines, size_t_array_t* args,  size_t max_a
 		return false;
 	}
 	line_t* arg_1 = lineload(lines, args->items[0]);
-	res->A = arg_1->val_set ? arg_1->val_set : (set_t*) arg_1->val_rel;
+
+	// if line is not a set, throw error
+	if (!(res->A = arg_1->val_set)) {
+		invalid_argument(arg_1);
+	}
+
 	if (args->len >= 2) {
 		res->N = args->items[1];
 	}
 	if (args->len > max_args) {
 		throw_error("Too many arguments: %ld > %ld", args->len, max_args);
 	}
-	if (res->A) {
-		return true;
-	} else {
-		invalid_argument(arg_1);
-		return false;
-	}
+	return (res->A != NULL);
 }
 
 bool arg_AB_val(arg_AB_t* res, array_t* lines, size_t_array_t* args, size_t max_args) {
