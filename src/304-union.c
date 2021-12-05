9860d8661640e5df935a66d56eb7735c1c15acb9
stefanpeknik <stefa9982@seznam.cz>
2021-12-05 17:36:54 +0100

    304-union.c:  add comm + rename set variable

--- src/304-union.c
+++ src/304-union.c
@@ -1,15 +1,14 @@
-//
-// Created by Štefan Pekník on 19.11.2021.
-//
-
 #include <stdbool.h>
 #include <stdio.h>
 
+// creates new set uni_set which contains all elements from set1,
+// goes through all elements in set2 and
+// adds every new element (element which is not in set uni_set) to set uni_set
 set_t* cmd_union(set_t* set1, set_t* set2) {
-    set_t *setu = set1;
+    set_t *uni_set = set1;
     for (size_t i = 0; i < set2->number_of_elements; i++) {
-        if (!is_element_in_set(set2->elements[i], setu))
-            add_element_to_set(set2->elements[i], &setu);
+        if (!is_element_in_set(set2->elements[i], uni_set))
+            add_element_to_set(set2->elements[i], &uni_set);
     }
-    return setu;
+    return uni_set;
 }
